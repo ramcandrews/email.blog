6ed8affc2e91ad54a467743b5db54210e056a191
Ryan McAndrews <ramcandrews@users.noreply.github.com>
2020-03-15 22:27:49 +0900

    switched from user input to a secrets file
    switched from user input to a secrets file. Also switched from all print statements to print statements and saving to a JSON file.

--- imap_connect.py
+++ imap_connect.py
@@ -1,21 +1,20 @@
 import imaplib
 import email.message
+import json
+
+import secrets
 
 from pprint import pprint
 
-msg_number = int()
-server = input("server address:")
-username =input("username:")
-password = input("password:")
-port = int()
-
 # Connect to the server
-print('Connecting to ', server)
-connection = imaplib.IMAP4_SSL(server, port)
+print('Connecting to ', secrets.server)
+connection = imaplib.IMAP4_SSL(secrets.server, secrets.port)
 
 # Login to our account
-print('Logging in as ', username)
-connection.login(username, password)
+print('Logging in as ', secrets.username)
+connection.login(secrets.username, secrets.password)
+
+msg_number = int()
 
 try:
     typ, data = connection.list()
@@ -27,21 +26,32 @@ try:
     print(typ, data)
     num_msgs = int(data[0])
     print('There are %d messages in Drafts' % num_msgs)
-    for message in range(5):
-        msg_number += 1
-        typ, msg_data = connection.fetch("{}".format(msg_number), '(RFC822)')
-        for response_part in msg_data:
-            if isinstance(response_part, tuple):
-                meta = email.message_from_string(response_part[1].decode("utf-8"))
-                for header in [ 'to', 'from', 'subject' ]:
-                    print('%-8s: %s' % (header.upper(), meta[header]))
-                if meta.is_multipart():
-                    print('BODY: ', meta.get_payload()[0]) #plain
-                    #print(meta.get_payload()[1]) #html                
-                else:
-                    print('BODY: ', meta.get_payload(None, True))
+
+    theDict = dict()
+    with open("dump_file.json", "w") as write_file:
+    
+
+        for message in range(num_msgs):
+            msg_number += 1
+            theDict[msg_number] = list()
+            typ, msg_data = connection.fetch("{}".format(msg_number), '(RFC822)')
+            for response_part in msg_data:
+                if isinstance(response_part, tuple):
+                    meta = email.message_from_string(response_part[1].decode("utf-8"))
+                    for header in [ 'to', 'from', 'subject' ]:
+                        theDict[msg_number].append(meta[header])
+                    if str(meta.is_multipart()):
+                        try:
+                            theDict[msg_number].append(str(meta.get_payload()[0])) #plain
+                            theDict[msg_number].append(str(meta.get_payload()[1])) #html
+                        except:
+                            theDict[msg_number].append(None)
+                    else:
+                        theDict[msg_number].append(meta.get_payload(None, True))
+            print(theDict[msg_number])
+        json.dump(theDict, write_file)
 finally:
-    try:
+    try:       
         connection.close()
     except:
         pass
